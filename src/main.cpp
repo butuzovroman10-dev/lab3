d49332b27307d708b58a1a8b3be14e67d7fe466f
butuz20072009-eng <butuz20072009@gmail.com>
2025-10-02 12:53:13 +0300

    Ромка2

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "matrix.h"
+
+int main() {
+    try{
+        std::string input;
+        std::string result;
+        std::cout << "Введите строки (для завершения введите 'end'):" << std::endl;
+        char a=0;
+        while (true){
+            std::getline(std::cin, input);
+            
+        }
+
+    }
+    catch(...){
+
+    }
+}
