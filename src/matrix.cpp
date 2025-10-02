2f7f29c4a0a0062d207fa4f12ba3a058e626eacd
Sheband <oochov@gmail.com>
2025-10-02 12:32:04 +0300

    String to matrix

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,1 +1,25 @@
-010101
+#include "matrix.h"
+
+
+
+Matrix string_to_matrix(char* text, int rows, int cols){
+    Matrix mat;
+    mat.rows = rows;
+    mat.cols = cols;
+
+
+    mat.data = new char*[rows];
+    if (mat.data = nullptr){
+        throw std::bad_alloc();
+    }
+
+    for (int i = 0; i < mat.rows; i++){
+        mat.data[i] = new char[cols];
+        for (int j; j < mat.cols; j ++){
+            mat.data[i][j] = text[i * mat.cols + j];
+        }
+
+    }
+    
+    return mat;
+}
